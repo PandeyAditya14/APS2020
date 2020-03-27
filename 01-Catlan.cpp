25b1d52c61fa18a7488483f3d920836e9aecf866
Aditya Kumar Pandey <aditya141199@gmail.com>
2020-03-27 20:18:52 +0530

    Binomial Coefficient Code

--- 01-Catlan.cpp
+++ 01-Catlan.cpp
@@ -1,9 +1,13 @@
-// This code is written by : Adity Kumar Pandey on 27/03/2020
-// This code is to demostrate different ways to calculate the nth catalan number here we demostrate two methods to do so
-// 1. First we use dp to store the values of previous catalan numbers to calculate the next catalan numbers (Efficiency O(n^2) )
-// 2. We use binomial Coefficient to calculate the nth catalan number (Efficiency of O(n) )
-// https://www.geeksforgeeks.org/program-nth-catalan-number/
-
+/**
+ * @ Author: Aditya Kumar Pandey
+ * @ Create Time: 2020-03-27 17:44:03
+ * @ Modified by: Aditya Kumar Pandey
+ * @ Modified time: 2020-03-27 18:23:06
+ * @ Description: This code is to demostrate different ways to calculate the nth catalan number here we demostrate two methods to do so
+ *                  1. First we use dp to store the values of previous catalan numbers to calculate the next catalan numbers (Efficiency O(n^2) )
+ *                  2. We use binomial Coefficient to calculate the nth catalan number (Efficiency of O(n) )
+ *                  https://www.geeksforgeeks.org/program-nth-catalan-number/
+ */
 #include<bits/stdc++.h>
 using namespace std;
 

69df2e58269fb9a08d55ebbb177f402da26967bc
Aditya Kumar Pandey <aditya141199@gmail.com>
2020-03-28 20:13:13 +0530

    LIS

---
+++ 09-LCS.cpp
@@ -0,0 +1,21 @@
+/**
+ * @ Author: Aditya Kumar Pandey
+ * @ Create Time: 2020-03-28 20:09:37
+ * @ Description:
+ * @ Link:
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(char *X , char *Y , int lenX , int lenY){
+    int **seq = new int*[lenX+1];
+    for (int i = 0 ; i<= lenX ; i++)  seq[i]  = new int[lenY];
+    
+
+}
+
+
+int main(){
+    return 0;
+}
